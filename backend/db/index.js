72916b62bacaf05801c85532330d0ab7dc779f18
mr-nobody11 <aswinvs542@gmail.com>
2023-05-26 16:57:13 +0530

    Fully Finished

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://ASWINVS:Aswin123@cluster0.1gjyfad.mongodb.net/')
+mongoose.connect('mongodb+srv://ASWINVS:Aswin123@cluster0.1gjyfad.mongodb.net/Products')
     .then(() => {
         console.log('MongoDb Atlas connection established')
     })
