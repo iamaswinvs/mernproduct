17504b924a74ab01d68b1b1089251af5a0d7d1f3
mr-nobody11 <aswinvs542@gmail.com>
2023-05-26 16:36:58 +0530

    Finished

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,18 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';//!for routing
+const mongoose = require('mongoose')
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        {/* Add Browser Router  */}
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>
-);
-
-// If you want to start measuring performance in your ap
+mongoose.connect('mongodb+srv://ASWINVS:Aswin123@cluster0.1gjyfad.mongodb.net/')
+    .then(() => {
+        console.log('MongoDb Atlas connection established')
+    })
+    .catch(err => {
+        console.log(err)
+    })
